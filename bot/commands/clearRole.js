b946c17ccedadcd361cf0ebedded61558a61a076
Fiona <fiona.greenwood.sheridan@gmail.com>
2020-02-03 23:30:42 +1100

    Added clear command

---
+++ bot/commands/clearRole.js
@@ -0,0 +1,23 @@
+const {guilds} = require("../schemas")
+const {getGuildDoc} = require ("../utils")
+
+module.exports = {
+    name: 'clear',
+    aliases: [],
+    description: 'Removes all self assignable roles from you',
+    usage: ['clear'],
+    catagory: "Self Assign",
+    hidden: false,
+    owner: false,
+    userPerms: [],
+    runPerms: ["MANAGE_ROLES"],
+	async execute(message, args) {
+        
+        let guild = await getGuildDoc(message.guild.id)
+        let roles = message.guild.roles.filter(role => guild.roles.has(role.id))
+
+        message.member.roles.remove(roles)
+
+        return message.channel.send(`Cleared your roles`)
+    }
+};
