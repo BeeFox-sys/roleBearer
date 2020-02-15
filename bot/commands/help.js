92932ea9b82c05513a8bbfc29ed0527966aaec63
Fiona <fiona.greenwood.sheridan@gmail.com>
2020-02-15 11:04:49 +1100

    Better Help

--- bot/commands/help.js
+++ bot/commands/help.js
@@ -11,29 +11,38 @@ module.exports = {
     userPerms: [],
     runPerms: [],
 	async execute(message, args) {
-        try{
-            const data = [];
-            data.push('**__Command List__**');
-            let catagories = {
-                "Self Assign": "\n*Assign yourself roles*",
-                "Profiles": "\n*Save your assigned roles and load them*",
-                "Self Assign Management": "\n*Manage the assigned roles*",
-                "Misc": ""
-            }
-            for (const commandObj of Client.commands) {
-                let command = commandObj[1]
-                catagories[command.catagory] += `\n${Client.config.prefix}**${command.name}** - ${command.description.split("\n")[0]}`
-            }
-            for (const catagoryName in catagories) {
-                if (catagories.hasOwnProperty(catagoryName)) {
-                    const catagory = catagories[catagoryName];
-                    data.push(`\n\n__${catagoryName}__${catagory}`)
+        if(args.length > 0) {
+            try{
+                let command = Client.commands.get(args.join(" ").toLowerCase())
+                if(!command) return message.channel.send(`Command ${args.join(" ")} not found!`)
+                let commandHelp = `**__${command.name}__**${command.aliases && command.aliases.length > 0 ? ` (*${command.aliases.join(", ")}*)` : ``}\n${command.description}\n__Usage__:\n${command.usage.join('\n')}`
+                return message.channel.send(commandHelp)
+            } catch (error) {throw error}
+        } else {
+            try{
+                const data = [];
+                data.push('**__Command List__**');
+                let catagories = {
+                    "Self Assign": "\n*Assign yourself roles*",
+                    "Profiles": "\n*Save your assigned roles and load them*",
+                    "Self Assign Management": "\n*Manage the assigned roles*",
+                    "Misc": ""
                 }
-            }
-
-            data.push("\n\nWords in <angle brackets> are *required parameters*.\nWords in [square brackets] are *optional parameters*.\n**Note that you should not include the brackets in the actual command.**")
-            data.push(`\nYou can send \`${Client.config.prefix}help [command name]\` to get info on a specific command!`);
-            message.channel.send(data.join(" "))
-        } catch (error) {throw error}
+                for (const commandObj of Client.commands) {
+                    let command = commandObj[1]
+                    catagories[command.catagory] += `\n${Client.config.prefix}**${command.name}** - ${command.description.split("\n")[0]}`
+                }
+                for (const catagoryName in catagories) {
+                    if (catagories.hasOwnProperty(catagoryName)) {
+                        const catagory = catagories[catagoryName];
+                        data.push(`\n\n__${catagoryName}__${catagory}`)
+                    }
+                }
+    
+                data.push("\n\nWords in <angle brackets> are *required parameters*.\nWords in [square brackets] are *optional parameters*.\n**Note that you should not include the brackets in the actual command.**")
+                data.push(`\nYou can send \`${Client.config.prefix}help [command name]\` to get info on a specific command!`);
+                message.channel.send(data.join(" "))
+            } catch (error) {throw error}
+        }
     }
 };
